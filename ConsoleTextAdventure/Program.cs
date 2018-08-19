88d5b16213f96dedae37ecb041604dfda47c43cb
Muresan Radu <MuresanRadu@users.noreply.github.com>
2018-08-19 14:20:45 +0300

    Add project files.

---
+++ ConsoleTextAdventure/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CorporateLockdown
+{
+    class Program
+    {
+        public static bool quit;
+
+        static void Main()
+        {
+            GameManager.ShowTitleScreen();
+            Level.Initialize();
+            GameManager.StartGame();
+
+            while (!quit)
+            {
+                CommandProcessor.ProcessCommand(Console.ReadLine());
+            }
+        }
+    }
+}
